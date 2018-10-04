cb9554ad40b4b1889718a805a8dbee2496e24cdf
Matt Kelly <matthew.joseph.kelly@gmail.com>
2018-10-03 23:57:11 -0400

    In-progress move to new types

--- cmd/get.go
+++ cmd/get.go
@@ -4,11 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/ghodss/yaml"
+	//"github.com/ghodss/yaml"
 	"github.com/go-resty/resty"
+	//"github.com/olekukonko/tablewriter"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	"github.com/containership/csctl/pkg/cloud/api/types"
 )
 
 // Flags
@@ -16,40 +19,43 @@ var (
 	outputFormat string
 )
 
-func getResource(url string) (string, error) {
+func getResource(url string, output interface{}) error {
 	token := viper.GetString("token")
 	if token == "" {
-		return "", errors.New("please provide a token")
+		return errors.New("please provide a token")
 	}
 
 	authHeader := fmt.Sprintf("JWT %s", token)
 
-	resp, err := resty.R().SetHeader("Authorization", authHeader).Get(url)
+	resp, err := resty.R().SetHeader("Authorization", authHeader).
+		SetResult(output).
+		Get(url)
+
 	if err != nil {
-		return "", errors.Wrap(err, "error requesting resource")
+		return errors.Wrap(err, "error requesting resource")
 	}
 
-	return string(resp.Body()), err
+	if resp.IsError() {
+		return errors.Errorf("Containership Cloud responded with status %d: %s", resp.StatusCode(), resp.Body())
+	}
+
+	return nil
 }
 
-func getOrganizations() (string, error) {
+func listOrganizations() ([]types.Organization, error) {
 	url := fmt.Sprintf("%s/v3/organizations", viper.Get("apiBaseURL"))
-	return getResource(url)
+	orgs := make([]types.Organization, 0)
+	return orgs, getResource(url, &orgs)
 }
 
-func getClusters(organizationID string) (string, error) {
-	url := fmt.Sprintf("%s/v3/organizations/%s/clusters", viper.Get("apiBaseURL"), organizationID)
-	return getResource(url)
-}
-
-func getNodePools(organizationID, clusterID string) (string, error) {
-	url := fmt.Sprintf("%s/v3/organizations/%s/clusters/%s/node-pools",
-		viper.Get("provisionBaseURL"), organizationID, clusterID)
-	return getResource(url)
+func getOrganization(id string) (*types.Organization, error) {
+	url := fmt.Sprintf("%s/v3/organizations/%s", viper.Get("apiBaseURL"), id)
+	var org types.Organization
+	return &org, getResource(url, &org)
 }
 
 // TODO this function is beyond terrible
-func outputResponse(resp string) {
+func outputResponse(resp interface{}) {
 	switch outputFormat {
 	case "", "table":
 		// Default
@@ -57,15 +63,7 @@ func outputResponse(resp string) {
 		fmt.Println(resp)
 
 	case "json":
-		respMap := make([]map[string]interface{}, 0)
-
-		err := json.Unmarshal([]byte(resp), &respMap)
-		if err != nil {
-			fmt.Printf("Error unmarshalling JSON: %v", err)
-			return
-		}
-
-		j, err := json.MarshalIndent(respMap, "", "  ")
+		j, err := json.MarshalIndent(resp, "", "  ")
 		if err != nil {
 			fmt.Printf("Error formatting JSON: %v", err)
 			return
@@ -73,23 +71,20 @@ func outputResponse(resp string) {
 
 		fmt.Println(string(j))
 
-		// TODO
-		fmt.Println("(output format %s not supported)", outputFormat)
-
 	case "yaml":
-		y, err := yaml.JSONToYAML([]byte(resp))
-		if err != nil {
-			fmt.Println("(Error converting to YAML)")
-			return
-		}
+		//y, err := yaml.JSONToYAML([]byte(resp))
+		//if err != nil {
+		//fmt.Println("(Error converting to YAML)")
+		//return
+		//}
 
-		fmt.Println(string(y))
+		//fmt.Println(string(y))
 
 	case "jsonpath":
 		fallthrough
 	default:
 		// TODO
-		fmt.Println("(output format %s not supported)", outputFormat)
+		fmt.Printf("(output format %s not supported)", outputFormat)
 	}
 }
 
@@ -101,47 +96,63 @@ var getCmd = &cobra.Command{
 
 TODO this is a long description`,
 
-	Args: cobra.ExactArgs(1),
+	Args: cobra.RangeArgs(1, 2),
 
 	Run: func(cmd *cobra.Command, args []string) {
 		resource := args[0]
 		switch resource {
 		case "org", "orgs", "organization", "organizations":
-			resp, err := getOrganizations()
+			var resp interface{}
+			var err error
+			if len(args) == 2 {
+				orgId := args[1]
+				resp, err = getOrganization(orgId)
+			} else {
+				resp, err = listOrganizations()
+			}
+
 			if err != nil {
 				fmt.Println(err)
-				return
+			} else {
+				outputResponse(resp)
 			}
 
-			outputResponse(resp)
+			/*
+				case "cluster", "clusters":
+					if organizationID == "" {
+						fmt.Println("organization is required")
+						return
+					}
 
-		case "cluster", "clusters":
-			if organizationID == "" {
-				fmt.Println("organization is required")
-				return
-			}
+					var resp interface{}
+					var err error
+					if len(args) == 2 {
+						clusterId := args[1]
+						resp, err = getCluster(orgId, clusterId)
+					} else {
+						resp, err = listClusters(orgId)
+					}
 
-			resp, err := getClusters(organizationID)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
+					if err != nil {
+						fmt.Println(err)
+					} else {
+						outputResponse(resp)
+					}
 
-			outputResponse(resp)
+						case "nodepool", "nodepools", "np", "nps":
+							if organizationID == "" || clusterID == "" {
+								fmt.Println("organization and cluster are required")
+								return
+							}
 
-		case "nodepool", "nodepools", "np", "nps":
-			if organizationID == "" || clusterID == "" {
-				fmt.Println("organization and cluster are required")
-				return
-			}
+							resp, err := getNodePools(organizationID, clusterID)
 
-			resp, err := getNodePools(organizationID, clusterID)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			outputResponse(resp)
+							if err != nil {
+								fmt.Println(err)
+							} else {
+								outputResponse(resp)
+							}
+			*/
 
 		default:
 			fmt.Println("Error: invalid resource specified: %q", resource)
