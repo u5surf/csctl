f231d5784a5efc33f9e257cff1b153ff01e6bba0
Matt Kelly <matthew.joseph.kelly@gmail.com>
2018-11-06 11:23:25 -0500

    Implement delete for current resources

--- cmd/delete.go
+++ cmd/delete.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+
+	"github.com/containership/csctl/resource"
 )
 
 // deleteCmd represents the delete command
@@ -17,9 +19,9 @@ TODO this is a long description`,
 	Args: cobra.RangeArgs(1, 2),
 
 	Run: func(cmd *cobra.Command, args []string) {
-		resource := args[0]
-		switch resource {
-		case "org", "orgs", "organization", "organizations":
+		resourceName := args[0]
+		switch {
+		case resource.Organization().HasAlias(resourceName):
 			id := args[1]
 			err := clientset.API().Organizations().Delete(id)
 			if err != nil {
@@ -28,22 +30,67 @@ TODO this is a long description`,
 				fmt.Printf("Organization %s successfully deleted\n", id)
 			}
 
-		case "cluster", "clusters":
+		case resource.CKECluster().HasAlias(resourceName):
 			if organizationID == "" {
 				fmt.Println("organization is required")
 				return
 			}
 
-			clusterID := args[1]
-			err := clientset.Provision().CKEClusters(organizationID).Delete(clusterID)
+			id := args[1]
+			err := clientset.Provision().CKEClusters(organizationID).Delete(id)
 			if err != nil {
 				fmt.Println(err)
 			} else {
-				fmt.Printf("Cluster %s successfully deleted\n", clusterID)
+				fmt.Printf("Cluster %s delete successfully initiated\n", clusterID)
 			}
 
+		case resource.NodePool().HasAlias(resourceName):
+			if organizationID == "" || clusterID == "" {
+				fmt.Println("organization and cluster are required")
+				return
+			}
+
+			id := args[1]
+			err := clientset.Provision().NodePools(organizationID, clusterID).Delete(id)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Printf("Node pool %s delete successfully initiated\n", id)
+			}
+
+		case resource.Plugin().HasAlias(resourceName):
+			if organizationID == "" || clusterID == "" {
+				fmt.Println("organization and cluster are required")
+				return
+			}
+
+			id := args[1]
+			err := clientset.API().Plugins(organizationID, clusterID).Delete(id)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Printf("Plugin %s successfully deleted\n", id)
+			}
+
+		case resource.Template().HasAlias(resourceName):
+			if organizationID == "" {
+				fmt.Println("organization is required")
+				return
+			}
+
+			id := args[1]
+			err := clientset.Provision().Templates(organizationID).Delete(id)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Printf("Template %s successfully deleted\n", id)
+			}
+
+		case resource.Account().HasAlias(resourceName):
+			fmt.Println("Error: accounts cannot be deleted")
+
 		default:
-			fmt.Printf("Error: invalid resource specified: %q\n", resource)
+			fmt.Printf("Error: invalid resource name specified: %q\n", resourceName)
 		}
 	},
 }
