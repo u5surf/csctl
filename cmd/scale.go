8fa87cd98d77b66adc42dc79842766ebc1a29f23
Matt Kelly <matthew.joseph.kelly@gmail.com>
2018-11-23 14:07:56 -0500

    Allow scale to 0, more error checking

--- cmd/scale.go
+++ cmd/scale.go
@@ -9,6 +9,8 @@ import (
 	"github.com/containership/csctl/cloud/provision/types"
 )
 
+const countUnset int32 = -1
+
 var (
 	upCount   int32
 	downCount int32
@@ -26,24 +28,14 @@ var scaleCmd = &cobra.Command{
 	PreRunE: nodePoolScopedPreRunE,
 
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if !exactlyOneSet(upCount, downCount, toCount) {
-			return errors.New("must specify exactly one of --up, --down, or --to")
-		}
-
 		np, err := clientset.Provision().NodePools(organizationID, clusterID).Get(nodePoolID)
 		if err != nil {
 			return errors.Wrapf(err, "getting node pool %q", nodePoolID)
 		}
 
-		targetCount := *np.Count
-
-		switch {
-		case upCount != 0:
-			targetCount += upCount
-		case downCount != 0:
-			targetCount -= upCount
-		case toCount != 0:
-			targetCount = upCount
+		targetCount, err := validateAndGetTargetCount(*np.Count, upCount, downCount, toCount)
+		if err != nil {
+			return err
 		}
 
 		req := types.ScaleNodePoolRequest{
@@ -61,15 +53,47 @@ var scaleCmd = &cobra.Command{
 	},
 }
 
+func validateAndGetTargetCount(from, up, down, to int32) (int32, error) {
+	if !exactlyOneSet(up, down, to) {
+		return 0, errors.New("must specify exactly one of --up, --down, or --to")
+	}
+
+	var target int32
+	switch {
+	case up != countUnset:
+		if up < 1 {
+			return 0, errors.New("can't scale up by less than 1 node")
+		}
+
+		target = from + up
+
+	case down != countUnset:
+		if down < 1 {
+			return 0, errors.New("can't scale down by less than 1 node")
+		}
+
+		target = from - down
+
+	case to != countUnset:
+		target = to
+	}
+
+	if target < 0 {
+		return 0, errors.New("can't scale below 0")
+	}
+
+	return target, nil
+}
+
 func exactlyOneSet(v1, v2, v3 int32) bool {
 	switch {
-	case v1 == 0 && v2 == 0 && v3 == 0:
+	case v1 == countUnset && v2 == countUnset && v3 == countUnset:
 		return false
-	case v1 != 0 && (v2 != 0 || v3 != 0):
+	case v1 != countUnset && (v2 != countUnset || v3 != countUnset):
 		return false
-	case v2 != 0 && (v1 != 0 || v3 != 0):
+	case v2 != countUnset && (v1 != countUnset || v3 != countUnset):
 		return false
-	case v3 != 0 && (v1 != 0 || v2 != 0):
+	case v3 != countUnset && (v1 != countUnset || v2 != countUnset):
 		return false
 	}
 
@@ -81,7 +105,7 @@ func init() {
 
 	bindCommandToNodePoolScope(scaleCmd, false)
 
-	scaleCmd.Flags().Int32VarP(&upCount, "up", "u", 0, "scale up by N nodes")
-	scaleCmd.Flags().Int32VarP(&downCount, "down", "d", 0, "scale down by N nodes")
-	scaleCmd.Flags().Int32VarP(&toCount, "to", "t", 0, "scale to N nodes")
+	scaleCmd.Flags().Int32VarP(&upCount, "up", "u", countUnset, "scale up by N nodes")
+	scaleCmd.Flags().Int32VarP(&downCount, "down", "d", countUnset, "scale down by N nodes")
+	scaleCmd.Flags().Int32VarP(&toCount, "to", "t", countUnset, "scale to N nodes")
 }
--- cmd/scale_test.go
+++ cmd/scale_test.go
@@ -6,15 +6,127 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type validateScaleTest struct {
+	from int32
+	up   int32
+	down int32
+	to   int32
+
+	shouldError bool
+	expected    int32
+	msg         string
+}
+
+var validateScaleTests = []validateScaleTest{
+	{
+		from: 1,
+		up:   countUnset,
+		down: 1,
+		to:   countUnset,
+
+		shouldError: false,
+		expected:    0,
+		msg:         "scale down to 0 is allowed",
+	},
+	{
+		from: 2,
+		up:   countUnset,
+		down: countUnset,
+		to:   0,
+
+		shouldError: false,
+		expected:    0,
+		msg:         "scale target 0 is allowed",
+	},
+	{
+		from: 2,
+		up:   -2,
+		down: countUnset,
+		to:   countUnset,
+
+		shouldError: true,
+		msg:         "scale up by less than 1",
+	},
+	{
+		from: 2,
+		up:   countUnset,
+		down: -2,
+		to:   countUnset,
+
+		shouldError: true,
+		msg:         "scale down by less than 1",
+	},
+	{
+		from: 2,
+		up:   countUnset,
+		down: 3,
+		to:   countUnset,
+
+		shouldError: true,
+		msg:         "scale down below 0",
+	},
+	{
+		from: 2,
+		up:   countUnset,
+		down: countUnset,
+		to:   -1,
+
+		shouldError: true,
+		msg:         "scale target below 0",
+	},
+	{
+		from: 2,
+		up:   3,
+		down: countUnset,
+		to:   countUnset,
+
+		shouldError: false,
+		expected:    5,
+		msg:         "scale up by N",
+	},
+	{
+		from: 5,
+		up:   countUnset,
+		down: 2,
+		to:   countUnset,
+
+		shouldError: false,
+		expected:    3,
+		msg:         "scale down by N",
+	},
+	{
+		from: 5,
+		up:   countUnset,
+		down: countUnset,
+		to:   2,
+
+		shouldError: false,
+		expected:    2,
+		msg:         "scale to target",
+	},
+}
+
+func TestValidateAndGetTargetCount(t *testing.T) {
+	for _, c := range validateScaleTests {
+		target, err := validateAndGetTargetCount(c.from, c.up, c.down, c.to)
+		if c.shouldError {
+			assert.Error(t, err)
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, c.expected, target)
+		}
+	}
+}
+
 func TestExactlyOneSet(t *testing.T) {
-	assert.False(t, exactlyOneSet(0, 0, 0))
+	assert.False(t, exactlyOneSet(countUnset, countUnset, countUnset))
 
-	assert.True(t, exactlyOneSet(2, 0, 0))
-	assert.True(t, exactlyOneSet(0, 3, 0))
-	assert.True(t, exactlyOneSet(0, 0, 4))
+	assert.True(t, exactlyOneSet(2, countUnset, countUnset))
+	assert.True(t, exactlyOneSet(countUnset, 3, countUnset))
+	assert.True(t, exactlyOneSet(countUnset, countUnset, 4))
 
-	assert.False(t, exactlyOneSet(2, 0, 4))
-	assert.False(t, exactlyOneSet(0, 3, 4))
-	assert.False(t, exactlyOneSet(2, 3, 0))
+	assert.False(t, exactlyOneSet(2, countUnset, 4))
+	assert.False(t, exactlyOneSet(countUnset, 3, 4))
+	assert.False(t, exactlyOneSet(2, 3, countUnset))
 	assert.False(t, exactlyOneSet(2, 3, 4))
 }
