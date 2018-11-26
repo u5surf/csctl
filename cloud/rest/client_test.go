8bbe4e71457a199a275d344e33e96b3ef1065c45
Matt Kelly <matthew.joseph.kelly@gmail.com>
2018-11-25 22:46:05 -0500

    Add REST client tests

--- cloud/rest/client_test.go
+++ cloud/rest/client_test.go
@@ -1,11 +1,15 @@
 package rest
 
 import (
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/go-resty/resty"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/containership/csctl/cloud/api/types"
 )
 
 const (
@@ -14,6 +18,9 @@ const (
 
 	// Valid base URL
 	containershipURL = "https://api.containership.io"
+
+	// Valid path
+	testPath = "/v3/organizations/a5df7ade-f185-40a0-a379-0324af86b491"
 )
 
 // TODO tests for REST actions
@@ -24,7 +31,7 @@ func TestNewClient(t *testing.T) {
 		BaseURL: containershipURL,
 		Token:   validJWT,
 	})
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	assert.NotNil(t, client)
 
 	// BaseURL is required
@@ -47,6 +54,142 @@ func TestNewClient(t *testing.T) {
 		Token:   "",
 	})
 	assert.NotNil(t, err)
+}
+
+func TestGet(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method, "correct HTTP method used")
+	}))
+	defer ts.Close()
+
+	client, err := NewClient(Config{
+		BaseURL: ts.URL,
+		Token:   validJWT,
+	})
+	assert.NoError(t, err)
+
+	var out types.Organization
+	err = client.Get(testPath, &out)
+	assert.NoError(t, err)
+}
+
+func TestDelete(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "DELETE", r.Method, "correct HTTP method used")
+	}))
+	defer ts.Close()
+
+	client, err := NewClient(Config{
+		BaseURL: ts.URL,
+		Token:   validJWT,
+	})
+	assert.NoError(t, err)
+
+	err = client.Delete(testPath)
+	assert.NoError(t, err)
+}
+
+func TestPost(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method, "correct HTTP method used")
+	}))
+	defer ts.Close()
+
+	client, err := NewClient(Config{
+		BaseURL: ts.URL,
+		Token:   validJWT,
+	})
+	assert.NoError(t, err)
+
+	var body, out interface{}
+	err = client.Post(testPath, body, out)
+	assert.NoError(t, err)
+}
+
+func TestPatch(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "PATCH", r.Method, "correct HTTP method used")
+	}))
+	defer ts.Close()
+
+	client, err := NewClient(Config{
+		BaseURL: ts.URL,
+		Token:   validJWT,
+	})
+	assert.NoError(t, err)
+
+	var body, out interface{}
+	err = client.Patch(testPath, body, out)
+	assert.NoError(t, err)
+}
+
+func testServerWithStatus(status int) (*httptest.Server, *Client, error) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(status)
+	}))
+
+	client, err := NewClient(Config{
+		BaseURL: ts.URL,
+		Token:   validJWT,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return ts, client, nil
+}
+
+func testServerNonEmptyBodyChecker(t *testing.T) (*httptest.Server, *Client, error) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		assert.NoError(t, err, "body is readable")
+		assert.NotEmpty(t, body, "non-empty body")
+	}))
+
+	client, err := NewClient(Config{
+		BaseURL: ts.URL,
+		Token:   validJWT,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return ts, client, nil
+}
+
+func TestExecute(t *testing.T) {
+	ts, client, err := testServerWithStatus(http.StatusOK)
+	assert.NoError(t, err)
+	defer ts.Close()
+
+	err = client.Get("%%", nil)
+	assert.Error(t, err, "bad path results in error")
+
+	ts, client, err = testServerWithStatus(http.StatusInternalServerError)
+	assert.NoError(t, err)
+	defer ts.Close()
+
+	var out types.Organization
+	err = client.Get(testPath, &out)
+	assert.Error(t, err, "server error results in client error")
+
+	ts, client, err = testServerWithStatus(http.StatusNotFound)
+	assert.NoError(t, err)
+	defer ts.Close()
+
+	err = client.Get(testPath, &out)
+	assert.Error(t, err, "not found error results in client error")
+	nfErr, ok := err.(HTTPError)
+	assert.True(t, ok, "not found error results in HTTPError type")
+	assert.True(t, nfErr.IsNotFound(), "not found error results in 'not found' error type")
+
+	ts, client, err = testServerNonEmptyBodyChecker(t)
+	assert.NoError(t, err)
+	defer ts.Close()
+	body := types.Organization{
+		ID: "a5df7ade-f185-40a0-a379-0324af86b491",
+	}
+	err = client.Post(testPath, &body, &out)
 }
 
 func TestHttpErrorFromResponse(t *testing.T) {
